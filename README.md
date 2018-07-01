ad079179f781ab045905cd594a231f1c67ca879f
caffeinated-tech <liam@caffeinated.tech>
2018-07-01 17:14:33 +0100

    basic working pagination plugin

--- README.md
+++ README.md
@@ -1,2 +1,79 @@
-# mongoose-pagination
+# mongoose-fast-pagination
 A mongoose plugin for efficient query pagination
+
+
+## Install
+
+```
+    npm i mongoose-fast-pagination
+```
+
+## Usage
+
+```
+const mongoose = require('mongoose')
+const mongooseFastPagination = require('mongoose-fast-pagination')
+
+// create basic schema
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  }
+}, {
+  timestamps: true
+})
+
+// add indexes
+userSchema.index({
+  name: 1,
+  createdAt: 1
+})
+
+// add pagination plugin - this will add the #paginate method to the model
+userSchema.plugin(mongooseFastPagination, {
+  perPage: 10
+})
+
+const User = mongoose.model('User', userSchema)
+
+// need to wrap this part in a self executing async function so we can
+//  use await rather than needing promises
+;(async function () {
+  // connect to DB
+  await mongoose.connect('mongodb://localhost/mongoose-fast-pagination-test')
+  mongoose.connection.db.dropDatabase()
+
+  // create some test users
+  for (let i = 0; i < 15; i++) {
+    await User.create({
+      firstName: 'john' + i,
+      lastName: 'doe'
+    })
+  }
+
+  // load first page of 10
+  let results = await User.paginate({
+    where: { lastName: 'doe' },
+    sort: { createdAt: 'asc' }
+  })
+  console.log('Got first page of users: ', results.documents)
+  console.log('Are there more pages to load? : ', results.hasMore)
+  let lastId = results.documents[results.documents.length - 1].id
+  console.log('\nNow going to fetch the next page of results after user ', lastId)
+
+  // load second page
+  results = await User.paginate({
+    where: { lastName: 'doe' },
+    sort: { createdAt: 'asc' },
+    lastId: lastId
+  })
+  console.log('Got second page of users: ', results.documents)
+  console.log('Are there more pages to load? : ', results.hasMore)
+  process.exit()
+})()
+```
---
+++ index.js
@@ -0,0 +1,83 @@
+
+const DefaultOptions = {
+  perPage: 100,
+  defaultSortOrder: {
+    _id: 1
+  },
+  includeTotalCount: false,
+  includeHasMore: true
+}
+
+module.exports = exports = function paginationPlugin (schema, options = {}) {
+  // Overwrite default options with user supplied options
+  options = Object.assign(DefaultOptions, options)
+  schema.statics.paginate = async function ({ where, sort, lastId = null }) {
+    let limit = options.perPage
+    let hasMore
+    let query
+    let lastDocument
+    let documents
+    let count
+    let totalCount
+    
+    // set the per page limit. If the hasMore flag is requested, increment by 1
+    //  so we can tell if there is another page of documents to fetch
+    if (options.includeHasMore) {
+      limit++
+    }
+
+    query = this.where(where).limit(limit).sort(sort)
+
+    if (options.includeTotalCount) {
+      totalCount = await this.where(where).count()
+    }
+    // Looking for a page starting after the document with the lastId. Load the 
+    //  model and update the query params to find the next page of documents 
+    if (lastId !== null && lastId !== undefined) {
+      lastDocument = await this.findById(lastId)
+      if (lastDocument === null) {
+        return {
+          documents: [],
+          hasMore: false,
+          count: 0,
+          totalCount: totalCount
+        }
+      }
+      
+      // To get the next page of documents, modify the query for each sort 
+      //  parameter so that it gets fields greater / less than the last document
+      // This means we don't need to deal with cursors. For complex queries, it
+      //  is advisable to only sort by indexed fields - but this is generally
+      //  the case anyway.  
+      for (let field of Object.keys(sort)) {
+        let order = sort[field]
+        if (order === 'asc' || order == 'ascending' || order === 1) {
+          // ascending order
+          query.gt(field, lastDocument[field])
+        } else {
+          // descending order
+          query.lt(field, lastDocument[field])
+        }
+      }
+    }
+
+    // execute query
+    documents = await query.exec()  
+    count = documents.length
+    if (options.includeHasMore) {
+      if (documents.length > options.perPage) {
+        hasMore = true
+        documents.pop()
+      } else {
+        hasMore = false
+      }
+    }
+
+    return {
+      documents: documents,
+      hasMore: hasMore,
+      count: count,
+      totalCount: totalCount
+    } 
+  }
+}
